e6d2b433d6fa53a17d61638c9ecb5b197153c036
Brendon McDonnell <brendon@nwpro.org>
2013-12-29 15:40:59 -0500

    12/29/13_f
    PRODUCTION:
    Added back campaign_contact validation to campaign.rb;
    Remove validated name from training_sheets.rb

--- app/models/training_sheet.rb
+++ app/models/training_sheet.rb
@@ -1,6 +1,6 @@
 class TrainingSheet < ActiveRecord::Base
   
-  validates :print_contact_name, presence: true
+  #validates :print_contact_name, presence: true
   validates :print_contact_email, format: /.+@.+\..+/i, length: { within: 5..50}, allow_blank: true
   validates :print_contact_phone, numericality: true, allow_blank: true
   
