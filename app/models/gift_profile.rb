75a7649a6f087ee707689f99411f69729d7db390
Brendon McDonnell <brendon@nwpro.org>
2013-12-29 16:32:04 -0500

    12/29/13_h
    Tweaked gift_profile.rb to init @tier_3_first_total

--- app/models/gift_profile.rb
+++ app/models/gift_profile.rb
@@ -15,6 +15,9 @@ class GiftProfile < ActiveRecord::Base
   def cumulative_total
     @cumulative_total
   end
+  def tier_3_first_total
+    @tier_3_first_total
+  end
   
   def families_total=(val)
     @families_total = val
@@ -25,6 +28,9 @@ class GiftProfile < ActiveRecord::Base
   end
   def cumulative_total=(val)
     @cumulative_total = val
+  end
+  def tier_3_first_total=(val)
+    @tier_3_first_total = val
   end
   
   private
