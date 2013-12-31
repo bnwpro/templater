42d4d7495801c6fa9d85ef6a4524757c5235332c
Brendon McDonnell <brendon@nwpro.org>
2013-12-31 15:28:40 -0500

    12/31/13_g
    Further image path tweak, spelling errors

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -3,6 +3,7 @@ module DocumentsHelper
   def get_image_path # Needs absolute path to resolve LOGO for Mercury & wicked_pdf
     #'http://localhost:3000/assets/images/logo_bw.png'
     path = Rails.public_path.join('assets', 'images', 'logo_bw.png')
-    return path
+    trim_path = path.gsub(/.*.\/*public/, '')
+    return trim_path
   end
 end
