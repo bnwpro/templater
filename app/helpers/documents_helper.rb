a83198a5d2c52ab3e534e2d63b152465f18a0032
Brendon McDonnell <brendon@nwpro.org>
2013-12-31 15:17:09 -0500

    12/31/13_f
    Tweak image path for logo

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,6 +1,8 @@
 module DocumentsHelper
   
   def get_image_path # Needs absolute path to resolve LOGO for Mercury & wicked_pdf
-    'http://localhost:3000/assets/images/logo_bw.png'
+    #'http://localhost:3000/assets/images/logo_bw.png'
+    path = Rails.public_path.join('assets', 'images', 'logo_bw.png')
+    return path
   end
 end
