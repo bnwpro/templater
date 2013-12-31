24b99c92b0480ec004cb5b356b475ed035cea242
Brendon McDonnell <brendon@nwpro.org>
2013-12-31 15:59:45 -0500

    12/31/13_i
    Added full URL to image path

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -2,8 +2,10 @@ module DocumentsHelper
   
   def get_image_path # Needs absolute path to resolve LOGO for Mercury & wicked_pdf
     #'http://localhost:3000/assets/images/logo_bw.png'
-    path = Rails.public_path.join('assets', 'images', 'logo_bw.png').to_s
-    trim_path = path.gsub(/.*.\/*public/, '')
-    return trim_path
+    #path = Rails.public_path.join('assets', 'images', 'logo_bw.png').to_s
+    #trim_path = path.gsub(/.*.\/*public/, '')
+    #image_path = 
+    #return trim_path
+    'http://templater-env-9mvpzgknsa.elasticbeanstalk.com/assets/images/logo_bw.png'
   end
 end
