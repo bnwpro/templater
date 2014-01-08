bf270a5a975d4daddcecadf420a9a18d009d3cec
Brendon McDonnell <brendon@nwpro.org>
2014-01-07 22:11:44 -0500

    testing_2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Templater::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this). ## TESTING (was 'true')
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
