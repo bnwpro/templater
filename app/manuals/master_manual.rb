64c606a655a8a55120b72a720765b3734d7738e5
Brendon McDonnell <brendon@nwpro.org>
2014-01-08 00:09:50 -0500

    testing_8

--- app/manuals/master_manual.rb
+++ app/manuals/master_manual.rb
@@ -18,7 +18,78 @@ class MasterManual < Manual
         pdf.move_down 40
         pdf.text "Master Program Manual", :size => 32, :style => :bold, :align => :center
       end
-      #
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/common_pages.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/common_pages_enlist.pdf")
+      # "Advance Commitment Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/ac_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/ac.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/giving_potential.pdf")
+      # "Campaign Administrator Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/cadmin_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/cadmin_to_ws1.pdf")
+      Worksheets.new.add_cadmin_ws1_data(pdf, campaign)
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/cadmin_ws1_to_ws2.pdf")
+      Worksheets.new.add_cadmin_ws2_data(pdf, campaign)
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/cadmin_ws3.pdf")
+      Worksheets.new.add_cadmin_ws3_data(pdf, campaign)
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/cadmin_ws3_to_ws4.pdf")
+      Worksheets.new.add_cadmin_ws4_data(pdf, campaign)
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/cadmin_ws5.pdf")
+      Worksheets.new.add_cadmin_ws5_data(pdf, campaign)
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/cadmin_to_end.pdf")
+    # "Campaign Chair Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/chair_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/chair.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/giving_potential.pdf")
+    # "Children Activity Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/childs_act_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/childs_act.pdf")
+    # "Contact Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/contact_resp.pdf")
+    # "Event Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/event_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/event.pdf")
+    # "Follow Up Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/follow_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/follow.pdf")
+    # "Information Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/info_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/info.pdf")
+    # "Involvement Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/involve_resp.pdf")
+    # "Pacesetter Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/pacesetter_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/pacesetter.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/giving_potential.pdf")
+    # "Pastor Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/pastor_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/pastor.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/giving_potential.pdf")
+    # "Prayer Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/prayer_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/prayer.pdf")
+    # "Print Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/print_resp.pdf")
+    # "Visual Communications Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/visual_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/visual.pdf")
+    # "Youth Leader Guide"
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{campaign_dir}/resp/youth_resp.pdf")
+      PdfMerger.get_template_to_merge(pdf: pdf, path: "#{appendices_dir}/youth.pdf")
+      
+    # Worksheets
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/AC1-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/AC2-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/AC3-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/CT1-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/CT2-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/CT3-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/CT4-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/CT5-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/CT6-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/CT7-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/IN1-en.pdf")
+    #  PdfMerger.get_template_to_merge(pdf: pdf, path: "#{manual_campaign_dir}/IN2-en.pdf")
     end
   end
 end
