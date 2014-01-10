ca251e2723428d4698e65bd5cebcace5c1bce1ac
Brendon McDonnell <brendon@nwpro.org>
2014-01-10 09:59:17 -0500

    1/10/14_c
    Manual.rb back to default

--- app/manuals/manual.rb
+++ app/manuals/manual.rb
@@ -25,10 +25,8 @@ class Manual < Prawn::Document
     block_calendar = File.join(campaign_dir, "block_calendar_cid_#{campaign.id}.pdf")
     
     # TESTING AREA
-    Worksheets.new.create_worksheets(worksheets: worksheets_en,
-      manual_campaign_docs_dir: manual_campaign_docs_dir,
-      campaign: campaign)
-    return
+    
+    #return
     
     if (block_calendar)#File.exist?(block_calendar)
       
