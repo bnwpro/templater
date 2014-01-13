b65f1363ab9aa8815503119227c4449d96936cf7
Brendon McDonnell <brendon@nwpro.org>
2014-01-12 21:53:58 -0500

    1/12/14_a
    Added validation to prevent null values on number fields in Enlistment
    & TrainingSheets

--- app/models/enlistment.rb
+++ app/models/enlistment.rb
@@ -1,6 +1,10 @@
 class Enlistment < ActiveRecord::Base
   belongs_to :campaign
   
+  validates_each :ac_enlist, :childs_act_enlist, :spc_event_enlist, :contact_enlist, :contact_each_enlist, :info_enlist do |record, attr, value|
+    record.errors.add(attr, 'Enlistment quantity cannot be blank') if value == nil
+  end
+  
   def vir_var
     ac_enlist
   end
--- app/models/training_sheet.rb
+++ app/models/training_sheet.rb
@@ -4,5 +4,13 @@ class TrainingSheet < ActiveRecord::Base
   validates :print_contact_email, format: /.+@.+\..+/i, length: { within: 5..50}, allow_blank: true
   validates :print_contact_phone, numericality: true, allow_blank: true
   
+  validates_each :man_ac, :man_camp_admin, :man_camp_chair, :man_childs_act,
+    :man_contact, :man_follow_up, :man_info, :man_invlvm, :man_pace_gifts,
+    :man_prayer, :man_print_comm, :man_spc_event, :man_vis_comm, :man_youth,
+    :man_pastor, :ts_ac1, :ts_ac2, :ts_ac3, :ts_ct1, :ts_ct2, :ts_ct3,
+    :ts_ct4, :ts_ct5, :ts_ct6, :ts_ct7, :ts_it1, :ts_it2 do |record, attr, value|
+      record.errors.add(attr, 'Quantity of Training Sheets cannot be blank') if value == nil
+    end
+  
   belongs_to :campaign
 end
