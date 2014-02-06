ef233529730db649777887ecfb4bf07167ae7e78
Brendon McDonnell <brendon@nwpro.org>
2014-02-06 14:07:47 -0500

    2/6/14_b
    Finished Zip and upload to S3
    Still needs UI tweaks

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -65,8 +65,9 @@ class DocumentsController < ApplicationController
     @campaign_city = params[:campaign_city]
     @files_to_zip = params[:to_zip]
     #puts @campaign_name, @campaign_city, @files_to_zip
-    Zip.new.zip_docs(campaign_name: @campaign_name, campaign_city: @campaign_city, selected_files: @files_to_zip)
+    ZipDocs.new.zip_docs(campaign_name: @campaign_name, campaign_city: @campaign_city, selected_files: @files_to_zip)
     head :ok, :content_type => 'text/html'
+    #redirect_to :back
   end
   
   def show
---
+++ app/models/zip_docs.rb
@@ -0,0 +1,55 @@
+class ZipDocs
+  require 'rubygems'
+  require 'zip'
+  
+  def zip_docs(options = {})
+    campaign_name = options[:campaign_name]
+    campaign_city = options[:campaign_city]
+    files = options[:selected_files]
+    url = "https://s3.amazonaws.com"
+    _campaign_name_dir = "#{campaign_name.tr(" ", "_").dup}_#{campaign_city.tr(" ", "_").dup}"
+    company_suffix = "_ofwc"
+    s3_campaign_bucket = _campaign_name_dir.downcase + company_suffix
+    
+    s3 = AWS::S3.new
+    
+    bucket = s3.buckets[s3_campaign_bucket]
+    if bucket.exists?
+      temp_zip = File.join("tmp/pdfs.zip")
+      
+      # Save files to zip to tmp directory
+      files.each do |files_to_zip|
+        bucket.objects.each do |pdf|
+          if files_to_zip == pdf.key
+            pdffile = [pdf]
+            pdffile.each do |zf|
+              File.open(File.join("tmp/documents/#{pdf.key}"), 'wb') do |file|
+                zf.read do |chunk|
+                  file.write(chunk)
+                end
+              end
+            end
+          end
+        end
+      end
+      
+      # Zip files in tmp, then delete
+      Zip::File.open(temp_zip, Zip::File::CREATE) do |zipfile|
+        temp_files_to_zip = Dir.glob(File.join("tmp/documents/", '*'))
+        #puts temp_files_to_zip
+        temp_files_to_zip.each do |filename|
+          name = File.basename(filename)
+          zipfile.add(name, filename)
+        end
+      end
+      
+      # Send zip archive to S3 Bucket
+      S3Upload.new.send_to_s3(files: [temp_zip], owner: _campaign_name_dir)
+      
+      # Delete zip archive from tmp directory
+      FileUtils.rm Dir.glob(File.join("tmp/documents/", '*'))
+      FileUtils.rm temp_zip
+    end
+  end
+  
+end
