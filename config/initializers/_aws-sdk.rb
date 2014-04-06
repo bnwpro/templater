003b3849d21e1a1f66ab7d71b6a782446b2ed9e4
Brendon McDonnell <brendon@nwpro.org>
2014-04-06 10:19:16 -0400

    4/6/14_b
    C2

---
+++ config/initializers/_aws-sdk.rb
@@ -0,0 +1,5 @@
+AWS.config(
+  :access_key_id => 'AKIAJEKI32SC23T2AMFA',
+  :secret_access_key => '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',
+  :verify_response_body_content_length => false
+)
--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,5 +1,5 @@
 AWS.config(
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],#'AKIAJEKI32SC23T2AMFA',
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],#'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',
+  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
   :verify_response_body_content_length => false
 )
