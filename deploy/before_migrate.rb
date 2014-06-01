b0b207f1dcd26110b1d0134cb748ef990dcc3010
Brendon McDonnell <brendon@nwpro.org>
2014-06-01 10:24:54 -0400

    6/1/14_b

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,5 +3,6 @@ rails_env = new_resource.environment["RAILS_ENV"]
 execute "rake assets:precompile" do
   cwd release_path
   command "bundle exec rake assets:precompile"
+  command "bundle config build.nokogiri --use-system-libraries"
   environment "RAILS_ENV" => rails_env
 end
